3d58a5dd07d635c9368cef33a274dbb9c5812040
Fernando Barbosa <fbcbarbosa@gmail.com>
2018-12-05 17:27:38 -0200

    Small updates
    * Disable approle, not needed with k8s serviceaccount login
    * Add more info on logs
    * Use ":" instead of "?" for separator

--- src/init-token.sh
+++ src/init-token.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eo pipefail
 
@@ -20,35 +20,16 @@ fi
 echo "Getting auth token from Vault server: ${VAULT_ADDR}"
 
 # Login to Vault and so I can get an approle token
-VAULT_LOGIN_TOKEN=$(curl -ksS --request POST \
+VAULT_LOGIN_TOKEN=$(curl -sS --request POST \
   ${VAULT_ADDR}/v1/auth/${KUBERNETES_AUTH_PATH}/login \
   -H "Content-Type: application/json" \
   -d '{"role":"'"${VAULT_LOGIN_ROLE}"'","jwt":"'"${KUBE_SA_TOKEN}"'"}' | \
   jq -r 'if .errors then . else .auth.client_token end')
 validateVaultResponse 'vault login token' "${VAULT_LOGIN_TOKEN}"
 
-VAULT_ROLE_ID=$(curl -ksS --header "X-Vault-Token: ${VAULT_LOGIN_TOKEN}" \
-  ${VAULT_ADDR}/v1/auth/approle/role/${VAULT_LOGIN_ROLE}/role-id | \
-  jq -r 'if .errors then . else .data.role_id end')
-validateVaultResponse 'role id' "${VAULT_ROLE_ID}"
-
-VAULT_SECRET_ID=$(curl -ksS --header "X-Vault-Token: ${VAULT_LOGIN_TOKEN}" \
-  --request POST \
-  ${VAULT_ADDR}/v1/auth/approle/role/${VAULT_LOGIN_ROLE}/secret-id | \
-  jq -r 'if .errors then . else .data.secret_id end')
-validateVaultResponse 'secret id' "${VAULT_SECRET_ID}"
-
-APPROLE_TOKEN=$(curl -ksS --request POST \
-  --data '{"role_id":"'"$VAULT_ROLE_ID"'","secret_id":"'"$VAULT_SECRET_ID"'"}' \
-  ${VAULT_ADDR}/v1/auth/approle/login | \
-  jq -r 'if .errors then . else .auth.client_token end')
-validateVaultResponse 'approle id' "${APPROLE_TOKEN}"
-
-echo "export VAULT_TOKEN=${APPROLE_TOKEN}" > /env/variables
+echo "export VAULT_TOKEN=${VAULT_LOGIN_TOKEN}" > /env/variables
 
 #########################################################################
-
-echo "Getting secrets from Vault"
 
 # Get all environment variables prefixed with SECRET_ and retrieve the secret from vault based on the value
 INJECTED_SECRET_KEYS=$(printenv | grep '^SECRET_' | awk -F "=" '{print $1}')
@@ -58,12 +39,14 @@ do
  value=$(printenv ${key})
  ACTUAL_KEY=$(echo ${key} | sed 's/^SECRET_//g')
 
- vault_secret_key=$(echo ${value} |awk -F "?" '{print $1}')
- vault_data_key=$(echo ${value} |awk -F "?" '{print $2}')
+ vault_secret_key=$(echo ${value} |awk -F ":" '{print $1}')
+ vault_data_key=$(echo ${value} |awk -F ":" '{print $2}')
  [[ -z ${vault_data_key} ]] &&  vault_data_key=value
 
- LOOKUP_SECRET_RESPONSE=$(curl -ksS \
-    --header "X-Vault-Token: ${APPROLE_TOKEN}" \
+ echo "Getting key ${value} from vault as secret ${ACTUAL_KEY}"
+
+ LOOKUP_SECRET_RESPONSE=$(curl -sS \
+    --header "X-Vault-Token: ${VAULT_LOGIN_TOKEN}" \
     ${VAULT_ADDR}/v1/${vault_secret_key} | \
       jq -r 'if .errors then . else . end')
     validateVaultResponse "secret (${vault_secret_key})" "${LOOKUP_SECRET_RESPONSE}"
@@ -78,12 +61,7 @@ do
         fi
     fi
 
-    if [[ ${ACTUAL_KEY} =~ "^/creds/database" ]]; then
-      echo "export ${ACTUAL_KEY}_USERNAME=\"$(echo ${VALUE_OF_SECRET} | jq -r '.username')\"" >> /env/variables
-      echo "export ${ACTUAL_KEY}_PASSWORD=\"$(echo ${VALUE_OF_SECRET} | jq -r '.password')\"" >> /env/variables
-    else
-      echo "export ${ACTUAL_KEY}=\"${VALUE_OF_SECRET}\"" >> /env/variables
-    fi
+    echo "export ${ACTUAL_KEY}=\"${VALUE_OF_SECRET}\"" >> /env/variables
 done
 
 echo "export LEASE_IDS=${LEASE_IDS}" >> /env/variables
